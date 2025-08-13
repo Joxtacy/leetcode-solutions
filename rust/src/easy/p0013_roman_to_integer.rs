584d4e3da618e09250bf11c8a4ff899e5d275aec
Joxtacy <10127673+Joxtacy@users.noreply.github.com>
2025-08-13 15:44:40 +0200

    refactor(rust): move `Roman` to `utils`

--- rust/src/easy/p0013_roman_to_integer.rs
+++ rust/src/easy/p0013_roman_to_integer.rs
@@ -1,4 +1,4 @@
-use std::ops::{Add, Sub};
+use crate::utils::roman::Roman;
 
 pub struct Solution;
 
@@ -16,77 +16,6 @@ impl Solution {
                 acc + r
             }
         })
-    }
-}
-
-#[derive(PartialEq, PartialOrd, Clone, Copy)]
-#[repr(u16)]
-enum Roman {
-    I = 1,
-    V = 5,
-    X = 10,
-    L = 50,
-    C = 100,
-    D = 500,
-    M = 1000,
-}
-
-impl TryFrom<char> for Roman {
-    type Error = String;
-
-    fn try_from(c: char) -> Result<Self, Self::Error> {
-        match c {
-            'I' | 'i' => Ok(Roman::I),
-            'V' | 'v' => Ok(Roman::V),
-            'X' | 'x' => Ok(Roman::X),
-            'L' | 'l' => Ok(Roman::L),
-            'C' | 'c' => Ok(Roman::C),
-            'D' | 'd' => Ok(Roman::D),
-            'M' | 'm' => Ok(Roman::M),
-            _ => Err(format!("'{c}' is not a valid roman numeral")),
-        }
-    }
-}
-
-// Roman + i32
-impl Add<i32> for Roman {
-    type Output = i32;
-
-    fn add(self, other: i32) -> i32 {
-        self as i32 + other
-    }
-}
-
-// i32 + Roman
-impl Add<Roman> for i32 {
-    type Output = i32;
-
-    fn add(self, other: Roman) -> i32 {
-        self + other as i32
-    }
-}
-
-// Roman - i32
-impl Sub<i32> for Roman {
-    type Output = i32;
-
-    fn sub(self, other: i32) -> i32 {
-        self as i32 - other
-    }
-}
-
-// i32 - Roman
-impl Sub<Roman> for i32 {
-    type Output = i32;
-
-    fn sub(self, other: Roman) -> i32 {
-        self - other as i32
-    }
-}
-
-impl From<Roman> for i32 {
-    fn from(r: Roman) -> i32 {
-        r as i32
     }
 }
 
